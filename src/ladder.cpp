39d72bca39f3282cd96250849112feac38294c0a
Pranav Thurgam <thurgam.pranav@gmail.com>
2025-03-16 20:54:30 +0000

    implemented generate word ladder

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -39,3 +39,27 @@ bool edit_distance_within(const std::string& str1, const std::string& str2, int 
 bool is_adjacent(const string& word1, const string& word2) {
     return edit_distance_within(word1, word2, 1);
 }
+
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+    std::queue<vector<string>> ladder_queue;
+    ladder_queue.push({begin_word});
+    std::set<string> visited;
+    visited.insert(begin_word);
+    while (!ladder_queue.empty()) {
+        vector<string> ladder = ladder_queue.front();
+        ladder_queue.pop();
+        string last_word = ladder.back();
+        for (auto word: word_list) {
+            if (is_adjacent(last_word, word)) {
+                if (!visited.find(word)) {
+                    visited.insert(word);
+                    vector<string> new_ladder = ladder;
+                    new_ladder.push_back(word);
+                    if (word == end_word) return new_ladder;
+                    ladder_queue.push(new_ladder);
+                }
+            }
+        }
+    }
+    return vector<string>();
+}
