e1f9654d9a07444165850da95f02fa70b64a3394
Pranav Thurgam <thurgam.pranav@gmail.com>
2025-03-17 01:08:54 +0000

    implemented dijkstras main functionality

--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     Graph G;
-    file_to_graph("/home/pthurgam/ICS46/HW9/src/small.txt", G);
+    file_to_graph("/home/pthurgam/ICS46/HW9/src/medium.txt", G);
     vector<int> previous;
     vector<int> distances = dijkstra_shortest_path(G, 0, previous);
     for (int i = 0; i < G.numVertices; ++i) {
